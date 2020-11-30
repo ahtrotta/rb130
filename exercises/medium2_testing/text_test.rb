c876933e780e6730fbef045351413be18730c721
ahtrotta <ahtrotta@gmail.com>
2020-11-30 17:23:29 -0500

    wrote a test for the Text#word_count method

--- exercises/medium2_testing/text_test.rb
+++ exercises/medium2_testing/text_test.rb
@@ -10,7 +10,15 @@ class TextTest < Minitest::Test
     text = @file.read
     text_sub = text.gsub('a', 'e')
     obj = Text.new(text)
+
     assert_equal text_sub, obj.swap('a', 'e')
+  end
+
+  def test_word_count
+    text = @file.read
+    obj = Text.new(text)
+
+    assert_equal 72, obj.word_count
   end
 
   def teardown
