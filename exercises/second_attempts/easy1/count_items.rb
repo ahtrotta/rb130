350256302f98e5e28efef8d4a1a918175e0cb79d
ahtrotta <ahtrotta@gmail.com>
2021-01-06 17:48:38 -0500

    completed count items (again)

---
+++ exercises/second_attempts/easy1/count_items.rb
@@ -0,0 +1,12 @@
+def count(array)
+  array.reduce(0) do |count, element|
+    count + (yield(element) ? 1 : 0)
+  end
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
