f93ae6a38250856b91cc00fe125d518607c6209c
ahtrotta <ahtrotta@gmail.com>
2021-01-06 14:29:02 -0500

    completed further explorations for optional blocks

--- exercises/second_attempts/easy1/optional_blocks.rb
+++ exercises/second_attempts/easy1/optional_blocks.rb
@@ -1,7 +1,17 @@
-def compute
-  block_given? ? yield : 'Does not compute'
+#def compute
+#  block_given? ? yield : 'Does not compute'
+#end
+#
+#p compute { 5 + 3 } == 8
+#p compute { 'a' + 'b' } == 'ab'
+#p compute == 'Does not compute'
+
+# Further Exploration
+
+def compute(input)
+  block_given? ? yield(input) : 'Does not compute'
 end
 
-p compute { 5 + 3 } == 8
-p compute { 'a' + 'b' } == 'ab'
-p compute == 'Does not compute'
+p compute(10) { |num| num + 7 } == 17
+p compute('string') { |str| 'this is a ' + str } == 'this is a string'
+p compute(:symbol) == 'Does not compute'
